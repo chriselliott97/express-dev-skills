6b1bef46ee1095048b0346e51ad89da0feb7a3a3
chriselliott97 <chris.elliott6497@gmail.com>
2022-09-22 09:53:24 -0400

    render the list

---
+++ data/skills.js
@@ -0,0 +1,12 @@
+const skills = [
+  {text: 'Problem-Solving', obtain: true},
+  {text: 'Critical Thinking', obtain: true},
+  {text: 'Creative', obtain: true},
+  {text: 'JavaScript', obtain: true},
+  {text: 'CSS', obtain: true},
+  {text: 'HTML', obtain: true}
+]
+
+export {
+  skills
+}
---
+++ routes/skills.js
@@ -0,0 +1,19 @@
+import { Router } from 'express'
+import { skills } from '../data/skills.js'
+
+const router = Router()
+
+/* GET users listing. */
+// router.get('/', function(req, res) {
+//   res.send('respond with a resource')
+// })
+
+router.get('/', function(req, res) {
+  res.render('skills/index', {
+    skills: skills
+  })
+})
+
+export {
+  router
+}
